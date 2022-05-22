1c6e17615f96eae02f470be64dce330e1c9d9e4f
xenia-melman <xenia.melman@gmail.com>
2022-05-22 12:25:08 +0200

    added tree traversal -> extracting nodes, edges; added a simple script for testing purposes

--- parser_test.py
+++ parser_test.py
@@ -6,16 +6,28 @@ from parser import ASTVisitor, graph
 
 def parse_py(code):
     # TODO language check
-    # TODO fit the ASTVisitor class to tree-sitter structure
+    # language init
     PY_LANGUAGE = Language('./build/my-languages.so', 'python')
     python_parser = Parser()
     python_parser.set_language(PY_LANGUAGE)
+
+    # get a tree
     tree = python_parser.parse(bytes(code, "utf8"))
-    print(tree.root_node.sexp())
+
+    # traverse tree to get nodes & edges
+    nodes, edges = [], []
+    node = tree.root_node
+    nodes, edges = tree_traverser(node)
+    print(nodes)
     return
 
 
 def parse_r(code):
+    R_LANGUAGE = Language('./build/my-languages.so', 'r')
+    r_parser = Parser()
+    r_parser.set_language(R_LANGUAGE)
+    tree = r_parser.parse(bytes(code, "utf8"))
+    print(tree.root_node.sexp())
     return
 
 
@@ -36,8 +48,26 @@ def parse_ast(code):
     return nodes, edges
 
 
+def tree_traverser(node):
+    if node.child_count == 0:
+        return [node], []
+
+    result_nodes = list()
+    result_edges = list()
+
+    # recursive tree traversal
+    for child_node in node.children:
+        child_nodes, child_edges = tree_traverser(child_node)
+        result_nodes = result_nodes + child_nodes
+        result_edges = result_edges + child_edges
+        result_edges.append((node, child_node))
+
+    result_nodes.append(node)
+    return result_nodes, result_edges
+
+
 code = b''
-with open("sklearn_example.py", "rb") as file:
+with open("bernoulli.py", "rb") as file:
     code += file.read()  # async read chunk
 
 # print(code.decode('utf-8'))
