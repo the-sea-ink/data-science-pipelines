f0a8aa1171c89b0451449b0f87fdef77e8f8ec88
xenia-melman <xenia.melman@gmail.com>
2022-05-30 22:46:39 +0200

    changed graph type in bfs_tree_traverser() from nx.Graph() to NXGraph() for compatibility with ReGraph

--- networkx_graph_builder.py
+++ networkx_graph_builder.py
@@ -2,18 +2,18 @@ from tree_sitter import Language, Parser
 import networkx as nx
 from regraph import NXGraph, Rule, plot_rule
 
-def parse(prog_language, code):
 
-    #if prog_language != "python" and prog_language != "r":
-     #   print("Currently only Python and R are supported.")
-      #  return
+def parse(prog_language, code):
+    # if prog_language != "python" and prog_language != "r":
+    #   print("Currently only Python and R are supported.")
+    #  return
 
     language = Language('./build/my-languages.so', prog_language)
     parser = Parser()
     parser.set_language(language)
     tree = parser.parse(bytes(code, "utf8"))
 
-    return bfs_tree_traverser(tree.root_node, nx.Graph())
+    return bfs_tree_traverser(tree.root_node, NXGraph())
 
 
 # traverses parsed nodes with Breadth-first search algorithm and
@@ -29,7 +29,7 @@ def bfs_tree_traverser(root_node, G):
     queue.append(root_node)
 
     # add root_node to the graph
-    G.add_node(0, type=root_node.type, text=root_node.text, children=root_node.children, parent=root_node.parent)
+    G.add_node(0, attrs={"type": root_node.type, "text": root_node.text})
 
     # loop to visit each node
     while queue:
@@ -39,8 +39,7 @@ def bfs_tree_traverser(root_node, G):
             if child_node not in visited:
                 node_id += 1
                 # add child node to graph
-                G.add_node(node_id, type=child_node.type, text=child_node.text, children=child_node.children,
-                           parent=child_node.parent, parent_id=parent_id)
+                G.add_node(node_id, attrs={"type": child_node.type, "text": child_node.text, "parent_id":parent_id})
                 # add edge between parent_node and child_node
                 G.add_edge(parent_id, node_id)
 
