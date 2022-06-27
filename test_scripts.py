fbd8f8ab9efe8f7b683e0f2a14ec85c1275ad4b6
Ava Nedaei <nedaeiava@gmail.com>
2022-06-27 16:51:47 +0200

    adding the python exam with error

--- test_scripts.py
+++ test_scripts.py
@@ -21,13 +21,10 @@ class Python:
 
     code_2 = """
         import numpy as np
-        
         def perform_bernoulli_trials(n, p):
-        
-            # Initialize number of successes: n_success
-            n_success = 0
-        
-            return n_success
+        # Initialize number of successes: n_success
+        n_success = 0
+        return n_success
         """
 
     code_3 = """
@@ -35,7 +32,7 @@ class Python:
         import matplotlib.pyplot as plt
         import numpy as np
         import lib
-        
+
         def perform_bernoulli_trials(n, p):
         
             # Initialize number of successes: n_success
@@ -95,6 +92,226 @@ class Python:
         y_pred = sc_Y.inverse_transform(y_pred)
         print(y_pred)
         """
+
+    code_7 = """
+        from sklearn.preprocessing import StandardScaler
+        X_scaled = StandardScaler().fit_transform(X)
+        pca = decomposition.PCA(n_components=2)
+        X_pca_scaled = pca.fit_transform(X_scaled)
+        print('Projecting %d-dimensional data to 2D' % X_scaled.shape[1])
+        plt.figure(figsize=(12,10))
+        plt.scatter(X_pca_scaled[:, 0], X_pca_scaled[:, 1], c=df['diagnosis'], alpha=0.7, s=40);
+        plt.colorbar()
+        plt.title('PCA projection')
+        plt.style.use('seaborn-muted');
+    """
+    code_8 = """
+        # Import required libraries:
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn import linear_model
+    # Read the CSV file :
+    data = pd.read_csv(“Fuel.csv”)
+    data.head()
+    # Let’s select some features to explore more :
+    data = data[[“ENGINESIZE”,”CO2EMISSIONS”]]
+    # ENGINESIZE vs CO2EMISSIONS:
+    plt.scatter(data[“ENGINESIZE”] , data[“CO2EMISSIONS”] , color=”blue”)
+    plt.xlabel(“ENGINESIZE”)
+    plt.ylabel(“CO2EMISSIONS”)
+    plt.show()
+    # Generating training and testing data from our data:
+    # We are using 80% data for training.
+    train = data[:(int((len(data)*0.8)))]
+    test = data[(int((len(data)*0.8))):]
+    # Modeling:
+    # Using sklearn package to model data :
+    regr = linear_model.LinearRegression()
+    train_x = np.array(train[[“ENGINESIZE”]])
+    train_y = np.array(train[[“CO2EMISSIONS”]])
+    regr.fit(train_x,train_y)
+    # The coefficients:
+    print (“coefficients : “,regr.coef_) #Slope
+    print (“Intercept : “,regr.intercept_) #Intercept
+    # Plotting the regression line:
+    plt.scatter(train[“ENGINESIZE”], train[“CO2EMISSIONS”], color=’blue’)
+    plt.plot(train_x, regr.coef_*train_x + regr.intercept_, ‘-r’)
+    plt.xlabel(“Engine size”)
+    plt.ylabel(“Emission”)
+    # Predicting values:
+    # Function for predicting future values :
+    def get_regression_predictions(input_features,intercept,slope):
+     predicted_values = input_features*slope + intercept
+     return predicted_values
+    # Predicting emission for future car:
+    my_engine_size = 3.5
+    estimatd_emission = get_regression_predictions(my_engine_size,regr.intercept_[0],regr.coef_[0][0])
+    print (“Estimated Emission :”,estimatd_emission)
+    # Checking various accuracy:
+    from sklearn.metrics import r2_score
+    test_x = np.array(test[[‘ENGINESIZE’]])
+    test_y_ = np.array(test[[‘CO2EMISSIONS’]])
+    test_y = regr.predict(test_x)
+    print(“Mean absolute error: %.2f” % np.mean(np.absolute(test_y_ — test_y)))
+    print(“Mean sum of squares (MSE): %.2f” % np.mean((test_y_ — test_y) ** 2))
+    print(“R2-score: %.2f” % r2_score(test_y_ , test_y) )"""
+
+    code_9 = """
+        import numpy as np
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import ListedColormap
+    
+    # plotting the fgiure
+    plt.figure(figsize = (7,7))
+    
+    # assigning the input values
+    X_set, y_set = X_train, y_train
+    
+    # ploting the linear graph
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01), np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75, cmap = ListedColormap(('black', 'white')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    
+    # ploting scattered graph for the values
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'blue'))(i), label = j)
+    
+    # labeling the graph
+    plt.title('Purchased Vs Non-Purchased')
+    plt.xlabel('Salay')
+    plt.ylabel('Age')
+    plt.legend()
+    plt.show()
+        """
+
+    code_10 = """
+           from sklearn.datasets import load_iris
+    iris = load_iris()
+    
+    # Model (can also use single decision tree)
+    from sklearn.ensemble import RandomForestClassifier
+    model = RandomForestClassifier(n_estimators=10)
+    
+    # Train
+    model.fit(iris.data, iris.target)
+    # Extract single tree
+    estimator = model.estimators_[5]
+    
+    from sklearn.tree import export_graphviz
+    # Export as dot file
+    export_graphviz(estimator, out_file='tree.dot', 
+                    feature_names = iris.feature_names,
+                    class_names = iris.target_names,
+                    rounded = True, proportion = False, 
+                    precision = 2, filled = True)
+    
+    # Convert to png
+    from subprocess import call
+    call(['dot', '-Tpng', 'tree.dot', '-o', 'tree.png', '-Gdpi=600'])
+    
+    # Display in python
+    import matplotlib.pyplot as plt
+    plt.figure(figsize = (14, 18))
+    plt.imshow(plt.imread('tree.png'))
+    plt.axis('off');
+    plt.show();
+    """
+
+    code_11 = """
+        from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    scaler.fit(df_feat)
+    df_scaled = scaler.transform(df_feat)
+    df_scaled = pd.DataFrame(df_scaled,columns=df_feat.columns[:4])
+    df_preprocessed = pd.concat([df_scaled,dummies,target],axis=1)
+    df_preprocessed.head()
+        """
+    code_12 = """
+    from sklearn.cluster import KMeans
+    from sklearn.metrics import confusion_matrix,classification_report,accuracy_score
+    
+    kmeans = KMeans(3,init='k-means++')
+    kmeans.fit(df_preprocessed.drop('species',axis=1))
+    print(confusion_matrix(df_preprocessed.species,kmeans.labels_))"""
+
+    code_13 = """
+        from sklearn.model_selection import train_test_split
+    from sklearn.neighbors import KNeighborsClassifier
+    
+    # We need to split data for supervised learning models.
+    X_train, X_test, y_train, y_test = train_test_split(df_preprocessed.drop('species',axis=1),target,test_size=0.50)
+    
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(X_train,y_train)
+    preds_knn = knn.predict(X_test)
+    print(confusion_matrix(y_test,preds_knn))"""
+
+    code_14 = """
+    # explore gradient boosting number of trees effect on performance
+    from numpy import mean
+    from numpy import std
+    from sklearn.datasets import make_classification
+    from sklearn.model_selection import cross_val_score
+    from sklearn.model_selection import RepeatedStratifiedKFold
+    from sklearn.ensemble import GradientBoostingClassifier
+    from matplotlib import pyplot
+     
+    # get the dataset
+    def get_dataset():
+        X, y = make_classification(n_samples=1000, n_features=20, n_informative=15, n_redundant=5, random_state=7)
+        return X, y
+     
+    # get a list of models to evaluate
+    def get_models():
+        models = dict()
+        # define number of trees to consider
+        n_trees = [10, 50, 100, 500, 1000, 5000]
+        for n in n_trees:
+            models[str(n)] = GradientBoostingClassifier(n_estimators=n)
+        return models
+     
+    # evaluate a given model using cross-validation
+    def evaluate_model(model, X, y):
+        # define the evaluation procedure
+        cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+        # evaluate the model and collect the results
+        scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+        return scores
+     
+    # define dataset
+    X, y = get_dataset()
+    # get the models to evaluate
+    models = get_models()
+    # evaluate the models and store results
+    results, names = list(), list()
+    for name, model in models.items():
+        # evaluate the model
+        scores = evaluate_model(model, X, y)
+        # store the results
+        results.append(scores)
+        names.append(name)
+        # summarize the performance along the way
+        print('>%s %.3f (%.3f)' % (name, mean(scores), std(scores)))
+    # plot model performance for comparison
+    pyplot.boxplot(results, labels=names, showmeans=True)
+    pyplot.show()
+    """
+
+    code_15 = """
+    from sklearn.cluster import KMeans
+    cs = []
+    for i in range(1, 11):
+        kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+        kmeans.fit(X)
+        cs.append(kmeans.inertia_)
+    plt.plot(range(1, 11), cs)
+    plt.title('The Elbow Method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('CS')
+    plt.show()
+    """
 
 class R:
     code_1 = """
