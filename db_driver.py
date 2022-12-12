e2f2c0e855ce44c74cd02e9b1a3bb2f08cc0903c
xenia-melman <xenia.melman@gmail.com>
2022-12-12 18:23:20 +0100

    added knowledge base database

---
+++ db_driver.py
@@ -0,0 +1,37 @@
+import sqlite3
+import csv
+from models.Function import Function
+
+connection = sqlite3.connect("knowledge_base.db")
+cursor = connection.cursor()
+
+
+def init_db():
+    cursor.execute("drop table functions")
+    cursor.execute("drop table arguments")
+    cursor.execute("CREATE TABLE functions(module_name, id, title, description, link, PRIMARY KEY (module_name, id))")
+    cursor.execute(
+        "CREATE TABLE arguments(module_name, id, argument_name, argument_type, argument_position, default_value)")
+    return
+
+
+# todo adapt to ini any
+def init_pandas():
+    with open("knowledge_base/pandas_kb.csv", newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+        header = next(csvreader)
+        for row in csvreader:
+            # add function values
+            function = Function.parse_from_list(row)
+            cursor.execute("INSERT INTO functions VALUES(?, ?, ?, ?, ?)",
+                           ['pandas', function.index, function.name, function.description, function.link])
+            if function.args is not None:
+                for arg in function.args:
+                    cursor.execute("INSERT INTO arguments VALUES(?, ?, ?, ?, ?, ?)",
+                                   ['pandas', function.index, arg.name, arg.type, arg.position, arg.default_value])
+    connection.commit()
+    return
+
+
+init_db()
+init_pandas()
--- models/Function.py
+++ models/Function.py
@@ -23,38 +23,43 @@ class Function:
         self.link = link
         self.args = args
 
-
     def __str__(self):
         return f'{self.index}, {self.name}, {self.description}, {self.link}\n args: {self.args}'
 
     @staticmethod
-    def parse_from_string(input_string: str):
+    def parse_from_list(input_list: list):
         # parse all attributes but arguments
-        parsed_string = input_string.split(",")
-        index, name, description, link = parsed_string[0], parsed_string[1], parsed_string[2], parsed_string[3]
+        #parsed_string = input_list.split(",")
+        index, name, description, link = input_list[0], input_list[1], input_list[2], input_list[3]
         # parse arguments
-        parsed_args = parsed_string[4:]
-        # strip " at start and end of argument list
-        parsed_args[0] = parsed_args[0][1:]
-        parsed_args[-1] = parsed_args[-1][:-1]
+        parsed_args = input_list[4:][0]
+
+        if parsed_args == '':
+            return Function(index, name, description, link, None)
         arguments = list()
-        for arg in parsed_args:
+        i = 0
+        for arg in parsed_args.split(","):
             if "*args" in arg:
-                arguments.append(Function.Argument(arg, "args", parsed_args.index(arg), None))
+                arguments.append(Function.Argument(arg, "args", i, None))
             elif "**kwargs" in arg:
-                arguments.append(Function.Argument(arg, "kwargs", parsed_args.index(arg), None))
+                arguments.append(Function.Argument(arg, "kwargs", i, None))
             elif "=" in arg:
                 arg_name, arg_value = arg.split("=")[0], arg.split("=")[1]
-                arguments.append(Function.Argument(arg_name, "hyperparameter", parsed_args.index(arg), arg_value))
+                arguments.append(Function.Argument(arg_name, "hyperparameter", i, arg_value))
             else:
-                arguments.append(Function.Argument(arg, "positional_argument", parsed_args.index(arg), None))
+                arguments.append(Function.Argument(arg, "positional_argument", i, None))
+            i += 1
         # create function
         function = Function(index, name, description, link, arguments)
         return function
 
+
 def test():
+    f = ['2202', 'pandas.read_pickle', 'Load pickled pandas object (or any object) from file.', 'https://pandas.pydata.org/docs/reference/api/pandas.read_pickle.html', "filepath_or_buffer,compression='infer',storage_options=None"]
     s = "2,pandas.HDFStore.append,Append to Table in file.,https://pandas.pydata.org/docs/reference/api/pandas.HDFStore.append.html,\"key,value,format=None,axes=None,index=True,append=True,complib=None,complevel=None,columns=None,min_itemsize=None,nan_rep=None,chunksize=None,expectedrows=None,dropna=None,data_columns=None,encoding=None,errors='strict'\""
-    function = Function.parse_from_string(s)
+    function = Function.parse_from_list(f)
     print(function)
 
+
 test()
+
