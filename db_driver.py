cf6e12f7ac51aa7b5e7cac796d1379a15cf8f18f
xenia-melman <xenia.melman@gmail.com>
2022-12-26 18:05:22 +0100

    added sklearn to knowledge base

--- db_driver.py
+++ db_driver.py
@@ -9,35 +9,37 @@ cursor = connection.cursor()
 def init_db():
     cursor.execute("drop table functions")
     cursor.execute("drop table arguments")
-    cursor.execute("CREATE TABLE functions(module_name, id, title, description, link, PRIMARY KEY (module_name, id))")
+    cursor.execute("CREATE TABLE functions(function_id INTEGER PRIMARY KEY, module_name, function_title, description, link)")
     cursor.execute(
-        "CREATE TABLE arguments(module_name, title, argument_name, argument_type, argument_position, default_value)")
+        "CREATE TABLE arguments(function_id, argument_name, argument_type, argument_position, default_value, FOREIGN KEY(function_id) REFERENCES functions(function_id))")
     return
 
 
 # todo adapt to ini any
-def init_pandas():
-    with open("knowledge_base/pandas_kb.csv", newline='') as csvfile:
+def init_module(filename, module_name):
+    with open(filename, newline='') as csvfile:
         csvreader = csv.reader(csvfile)
         header = next(csvreader)
         for row in csvreader:
             # add function values
             function = Function.parse_from_list(row)
-            cursor.execute("INSERT INTO functions VALUES(?, ?, ?, ?, ?)",
-                           ['pandas', function.index, function.name, function.description, function.link])
+            cursor.execute("INSERT INTO functions(module_name, function_title, description, link) VALUES(?, ?, ?, ?)",
+                           [module_name, function.name, function.description, function.link])
+            added_function_id = cursor.lastrowid
             if function.args is not None:
                 for arg in function.args:
-                    cursor.execute("INSERT INTO arguments VALUES(?, ?, ?, ?, ?, ?)",
-                                   ['pandas', function.name, arg.name, arg.type, arg.position, arg.default_value])
+                    cursor.execute("INSERT INTO arguments VALUES(?, ?, ?, ?, ?)",
+                                   [added_function_id, arg.name, arg.type, arg.position, arg.default_value])
     connection.commit()
     return
 
 def test():
-    result = Function.parse_from_db(connection, cursor, "pandas", "pandas.HDFStore.append")
+    init_db()
+    init_module("knowledge_base/pandas.csv", "pandas")
+    init_module("knowledge_base/sklearn.csv", "sklearn")
+    result = Function.parse_from_db(cursor, "sklearn", "sklearn.utils.validation.has_fit_parameter")
     print(result)
     return
 
-#init_db()
-#init_pandas()
 test()
 connection.close()
--- models/Function.py
+++ models/Function.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class Function:
     class Argument:
 
@@ -18,51 +19,54 @@ class Function:
         def __repr__(self):
             return self.__str__()
 
-    def __init__(self, index, name, description, link, args):
-        self.index = index
+    def __init__(self, name, description, link, args):
         self.name = name
         self.description = description
         self.link = link
         self.args = args
 
     def __str__(self):
-        return f'{self.index}, {self.name}, {self.description}, {self.link}\n args: {self.args}'
+        return f'{self.name}, {self.description}, {self.link}\n args: {self.args}'
 
     @staticmethod
     def parse_from_list(input_list: list):
-        # parse all attributes but arguments
-        #parsed_string = input_list.split(",")
-        index, name, description, link = input_list[0], input_list[1], input_list[2], input_list[3]
-        # parse arguments
-        parsed_args = input_list[4:][0]
+        # extract all attributes but arguments
+        name, description, link = input_list[0], input_list[1], input_list[2]
+        # extract arguments
+        parsed_args = input_list[3:][0]
 
         if parsed_args == '':
-            return Function(index, name, description, link, None)
+            return Function(name, description, link, None)
         arguments = list()
         i = 0
         for arg in parsed_args.split(","):
-            if "*args" in arg:
-                arguments.append(Function.Argument(arg, "args", i, None))
-            elif "**kwargs" in arg:
-                arguments.append(Function.Argument(arg, "kwargs", i, None))
-            elif "=" in arg:
+            if "=" in arg:
                 arg_name, arg_value = arg.split("=")[0], arg.split("=")[1]
                 arguments.append(Function.Argument(arg_name, "hyperparameter", i, arg_value))
+            elif "*args" in arg or arg == "*":
+                arguments.append(Function.Argument(arg, "args", i, None))
+            elif "**" in arg:
+                arguments.append(Function.Argument(arg, "kwargs", i, None))
             else:
                 arguments.append(Function.Argument(arg, "positional_argument", i, None))
             i += 1
         # create function
-        function = Function(index, name, description, link, arguments)
+        function = Function(name, description, link, arguments)
         return function
 
     @staticmethod
     def parse_from_db(cursor, module_name, title):
-        cursor.execute("SELECT module_name, id, title, description, link FROM functions WHERE module_name = ? AND title =?", (module_name, title))
+        cursor.execute(
+            "SELECT function_id, module_name, function_title, description, link FROM functions WHERE module_name = ? AND function_title =?",
+            (module_name, title))
         func = cursor.fetchall()
-        cursor.execute("SELECT argument_name, argument_type, argument_position, default_value FROM arguments WHERE module_name = ? AND title =?", (module_name, title))
+        func_id = func[0][0]
+        cursor.execute(
+            "SELECT argument_name, argument_type, argument_position, default_value FROM arguments WHERE function_id = ?",
+            (func_id,))
         args = cursor.fetchall()
         if len(args) == 0 and len(func) != 0:
-            return Function(func[0][1], func[0][2], func[0][3], func[0][4], None)
+            return Function(func[0][1], func[0][2], func[0][3],  None)
         elif len(func) != 0:
             arguments = []
             for arg in args:
@@ -71,17 +75,17 @@ class Function:
                 else:
                     arguments.append(Function.Argument(arg[0], arg[1], arg[2], arg[3]))
         if len(func) != 0:
-            function = Function(func[0][1], func[0][2], func[0][3], func[0][4], arguments)
+            function = Function(func[0][1], func[0][2], func[0][3], arguments)
             return function
         return -1
 
 
 def test():
-    f = ['2202', 'pandas.read_pickle', 'Load pickled pandas object (or any object) from file.', 'https://pandas.pydata.org/docs/reference/api/pandas.read_pickle.html', "filepath_or_buffer,compression='infer',storage_options=None"]
-    s = "2,pandas.HDFStore.append,Append to Table in file.,https://pandas.pydata.org/docs/reference/api/pandas.HDFStore.append.html,\"key,value,format=None,axes=None,index=True,append=True,complib=None,complevel=None,columns=None,min_itemsize=None,nan_rep=None,chunksize=None,expectedrows=None,dropna=None,data_columns=None,encoding=None,errors='strict'\""
+    f = ['pandas.read_pickle', 'Load pickled pandas object (or any object) from file.',
+         'https://pandas.pydata.org/docs/reference/api/pandas.read_pickle.html',
+         "filepath_or_buffer,compression='infer',storage_options=None"]
+    s = "pandas.HDFStore.append,Append to Table in file.,https://pandas.pydata.org/docs/reference/api/pandas.HDFStore.append.html,\"key,value,format=None,axes=None,index=True,append=True,complib=None,complevel=None,columns=None,min_itemsize=None,nan_rep=None,chunksize=None,expectedrows=None,dropna=None,data_columns=None,encoding=None,errors='strict'\""
     function = Function.parse_from_list(f)
-    #print(function)
+    print(function)
 
-
-#test()
-
+test()
